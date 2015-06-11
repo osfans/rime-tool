d01e966bbeed55f98f14b3fe2c677f032dbba3b2
osfans <waxaca@163.com>
2015-06-11 17:18:33 +0800

    去重工具

---
+++ uniq.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) == 1:
+  print("Usage: %s filename" % sys.argv[0])
+  exit
+
+for fn in sys.argv[1:]:
+  s = ""
+  uniq=set()
+  f = open(fn, "r")
+  for line in f:
+    if line.startswith("#") or line.startswith("\n") or (line not in uniq):
+      s+=line
+      uniq.add(line)
+  f.close()
+  t = open(fn, "w")
+  t.write(s)
+  t.close()
