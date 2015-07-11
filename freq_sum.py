fea4a144d5d4be130c0d5b485e88b4eaa7792dd7
osfans <waxaca@163.com>
2015-07-11 19:16:23 +0800

    Auto detect unicode and gbk

--- freq_sum.py
+++ freq_sum.py
@@ -3,20 +3,33 @@
 #https://github.com/rime/home/issues/18
 
 from collections import defaultdict
-f = open("input.txt") #輸入文件
-t = open("output.txt", "w") #輸出文件
 
 d = defaultdict(int)
-for line in f:
-  line = line.strip()
-  if line:
-    fs = line.split()
-    if len(fs) == 2:
-      word, freq = fs
-      d[word] += int(freq) #累加
+enc = ""
+
+def freq_sum(encoding):
+  try:
+    d.clear()
+    f = open("input.txt", encoding=encoding) #輸入文件
+    for line in f:
+      line = line.strip()
+      if line:
+        fs = line.split()
+        if len(fs) == 2:
+          word, freq = fs
+          d[word] += int(freq) #累加
+    f.close()
+    global enc
+    enc = encoding
+    return True
+  except:
+    return False
+
+any(map(freq_sum, ["utf-8-sig", "utf-16", "gbk", "gb18030"]))
+print("file encoding:", enc)
 l = sorted(d.items(), key = lambda x: x[1], reverse = True) #排序
+t = open("output.txt", "w", encoding = enc) #輸出文件
 for word, freq in l:
   print("%s\t%d" % (word, freq), file = t) #輸出
 
-f.close()
 t.close()
