d24fc2b2c3dfbfd4a9b2e0bb74d3994dda3a36de
osfans <waxaca@163.com>
2015-08-20 10:38:06 +0800

    pylint

--- uniq.py
+++ uniq.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
+"去除重複的編碼行"
+
 import sys
 
 if len(sys.argv) == 1:
-  print("Usage: %s filename" % sys.argv[0])
-  exit(0)
+    print("Usage: %s filename" % sys.argv[0])
+    exit(0)
 
 for fn in sys.argv[1:]:
-  s = ""
-  uniq=set()
-  f = open(fn, "r")
-  for line in f:
-    if line.startswith("#") or line.startswith("\n") or line.startswith(" ") or (line not in uniq):
-      s+=line
-      uniq.add(line)
-  f.close()
-  t = open(fn, "w")
-  t.write(s)
-  t.close()
+    s = ""
+    uniq = set()
+    f = open(fn, "r")
+    for line in f:
+        if (line and line[0] in "#\n ") or (line not in uniq):
+            s += line
+            uniq.add(line)
+    f.close()
+    t = open(fn, "w")
+    t.write(s)
+    t.close()
