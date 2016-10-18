8cc4e58a16d0eefba452d71683a74f735bf588e0
osfans <waxaca@163.com>
2016-10-18 13:23:12 +0800

    去重輸入輸出

--- tools/uniq.py
+++ tools/uniq.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 "去除重複的編碼行"
 
-import sys
-
-if len(sys.argv) == 1:
-    print(__doc__)
-    print("Usage: %s filename" % sys.argv[0])
-    exit(0)
+INPUT_NAME = "input.txt" #輸入input.txt
+OUTPUT_NAME = "output.txt" #輸出output.txt
 
 ENC = "utf-8"
 ENCODINGS = ["utf-8-sig", "utf-16", "gbk", "gb18030"] #嘗試編碼列表
@@ -22,19 +18,17 @@ def open_file(filename):
 
 def main():
     "主程序"
-    for filename in sys.argv[1:]:
+    input_file = open_file(INPUT_NAME)
+    if input_file:
         uniq = set()
-        input_f = open_file(filename)
         lines = []
-        for line in input_f:
+        for line in input_file:
             key = line.rstrip().rsplit("\t", 1)[0] #比較前N-1列
             if (line and line[0] in "#\n ") or (key not in uniq):
                 lines.append(line)
                 uniq.add(key)
-        input_f.close()
-        output_f = open(filename, "w", encoding=ENC)
-        output_f.writelines(lines)
-        output_f.close()
+        input_file.close()
+        open(OUTPUT_NAME, "w", encoding=ENC).writelines(lines)
 
 if __name__ == "__main__":
     main()
